cfef1442cb6cde5bdbe00c91c2f07cd90aab106b
AndersonLucasN <122025096+AndersonLucasN@users.noreply.github.com>
2023-01-27 12:17:56 -0300

    aula 06 java script

---
+++ Curso JS/aula06/script.js
@@ -0,0 +1,8 @@
+var n1 = window.prompt('Digite um número:') 
+var n2 = window.prompt('Digite um número:')
+//var n3= n1+n2 esse tipo de operação de soma não ira funcionar pois o prompt funciona como string
+var n3 = Number(n1) + Number(n2)
+window.alert('A soma entre os número é: ' + n3) //essa é forma que se con
+
+
+
