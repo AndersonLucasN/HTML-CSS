595cf40b2ca1ca685723ad347332c4fbc7a8b927
AndersonLucasN <122025096+AndersonLucasN@users.noreply.github.com>
2023-02-01 13:06:03 -0300

    aula 11

--- ex10/javascript.js
+++ ex10/javascript.js
@@ -1,4 +1,8 @@
 var a = document.getElementById('area')
+a.addEventListener('click', clicar)
+a.addEventListener('mouseenter', entrar)
+a.addEventListener('mouseout', sair)
+
 function clicar() {
     a.innerText= 'clicou'
     a.style.background='red'
