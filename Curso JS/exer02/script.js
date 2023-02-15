9ee2128c93ab920197df6e2ef28c7d420d70fb8d
AndersonLucasN <122025096+AndersonLucasN@users.noreply.github.com>
2023-02-15 12:03:25 -0300

    att4

--- Curso JS/exer02/script.js
+++ Curso JS/exer02/script.js
@@ -1,13 +1,36 @@
-function verificar(){
-   var data= new Date()
+function verificar() {
+   var data = new Date()
    var ano = data.getFullYear()
    var fano = document.getElementById('txtano')
-   var res= document.querySelector('div#res')
-   if(fano.valeu.length == 0 || fano.value>ano){
-    window.alert('[Erro] Os dados estão incorrentos, por favor! Verifique novamente')
-   }else{
-    var fsex= document.getElementsByName('radsex')
-    var idade= ano - Number(fano.value)
-    res.innerHTML = "idade: ${idade}"
+   var res = document.querySelector('div#res')
+   if (fano.value.length == 0 || fano.value > ano) {
+      window.alert('[Erro] Os dados estão incorrentos, por favor! Verifique novamente')
+   } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var genero = ''
+      var img = document.createElement('img')
+      img.setAttribute('id', 'foto')
+      if (fsex[0].checked) {
+         genero = 'Homem'
+         if (idade >= 0 && idade < 10) {
+            img.setAttribute('src', 'foto-bebe-m.png')
+         } else if (idade < 21) {
+            img.setAttribute('src', 'foto-jovem-m.png')
+         }
+         else if (idade < 50) {
+            img.setAttribute('src', 'foto-adulto-m.png')
+         }
+         else {
+            img.setAttribute('src', 'foto-idoso-m.png')
+         }
+      } else if (fsex[1].checked) {
+         genero = 'Mulher'
+         if (idade >= 0 && idade < 10) {
+
+         }
+         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+
+      }
    }
 }
