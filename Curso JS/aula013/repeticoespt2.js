10ed1a3bf8701177efdf63a75df5bb7c1a5e6609
AndersonLucasN <122025096+AndersonLucasN@users.noreply.github.com>
2023-02-17 10:55:34 -0300

    exer04

--- Curso JS/aula013/repeticoespt2.js
+++ Curso JS/aula013/repeticoespt2.js
@@ -1,1 +1,11 @@
-var c=1
+/* var c=1 
+        while(c<=10){
+        console.log('bom dia')
+            c++
+} Essa estrutura de repetição pode ser substituída pelo For, vejamos abaixo:
+*/ 
+for(var c=1;c<=10;c++){
+
+console.log('bom dia')
+}
+console.log('FIM')
